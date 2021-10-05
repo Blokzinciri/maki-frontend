9c2e522a05d450d1c3c65ac5d787780158b0cbc5
jameschain <james.cordova423@gmail.com>
2021-10-05 12:48:41 -0400

    chore: fix typo

--- src/views/Ifos/components/IfoQuestions/config.ts
+++ src/views/Ifos/components/IfoQuestions/config.ts
@@ -2,14 +2,14 @@ const config = [
   {
     title: 'What’s the difference between a Basic Sale and Unlimited Sale?',
     description: [
-      'In the Basic Sale, every user can commit a maximum of about 100 USD worth of CAKE-HT LP Tokens. We calculate the maximum LP amount about 30 minutes before each IFO. The Basic Sale has no participation fee.',
-      'In the Unlimited Sale, there’s no limit to the amount of CAKE-HT LP Tokens you can commit. However, there’s a fee for participation: see below.',
+      'In the Basic Sale, every user can commit a maximum of about 100 USD worth of MAKI-HT LP Tokens. We calculate the maximum LP amount about 30 minutes before each IFO. The Basic Sale has no participation fee.',
+      'In the Unlimited Sale, there’s no limit to the amount of MAKI-HT LP Tokens you can commit. However, there’s a fee for participation: see below.',
     ],
   },
   {
     title: 'Which sale should I commit to? Can I do both?',
     description: [
-      'You can choose one or both at the same time! If you’re only committing a small amount, we recommend the Basic Sale first. Just remember you need a PancakeSwap Profile in order to participate.',
+      'You can choose one or both at the same time! If you’re only committing a small amount, we recommend the Basic Sale first. Just remember you need a MakiSwap Profile in order to participate.',
     ],
   },
   {
@@ -23,13 +23,13 @@ const config = [
   {
     title: 'Where does the participation fee go?',
     description: [
-      'We burn it. The CAKE-HT LP tokens from the participation fee will be decomposed. We will then use the HT portion to market buy the CAKE equivalent, and finally throw all of the CAKE into the weekly token burn.',
+      'We burn it. The MAKI-HT LP tokens from the participation fee will be decomposed. We will then use the HT portion to market buy the MAKI equivalent, and finally throw all of the MAKI into the weekly token burn.',
     ],
   },
   {
     title: 'How can I get an achievement for participating in the IFO?',
     description: [
-      'You need to contribute a minimum of about 10 USD worth of CAKE-HT LP Tokens to either sale.',
+      'You need to contribute a minimum of about 10 USD worth of MAKI-HT LP Tokens to either sale.',
       'You can contribute to one or both, it doesn’t matter: only your overall contribution is counted for the achievement.',
     ],
   },
